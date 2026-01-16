74f87c0d21177c0e392c011b8048ceb97798def6
wtomah <djuno@Williams-MacBook-Pro.local>
2026-01-16 08:46:16 -0600

    New Code

---
+++ nbastattracker/mainpage/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.template import loader
+
+# Create your views here.
+
+def mainpage(request):
+    template = loader.get_template('mainpage.html')
+    return HttpResponse(template.render())
